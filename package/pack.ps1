b5b6141740b773be830fd16a4db3f463639bbf18
Duong Lap Tran <duong.tran2@niteco.se>
2025-09-16 15:25:03 +0700

    patch for Umbraco 13.10.0

---
+++ package/pack.ps1
@@ -0,0 +1,11 @@
+﻿$Version = Read-Host -Prompt 'Version'
+try
+{
+	dotnet pack ../src/TruePeople.SharePreview/TruePeople.SharePreview.csproj -c Release -o ../packages -p:PackageVersion=$Version -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+	Write-Host "Succesfully created Nuget package with version $($Version)"
+}
+catch {
+	Write-Host "Error occured:";
+	Write-Host $_;
+	 $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
+}
